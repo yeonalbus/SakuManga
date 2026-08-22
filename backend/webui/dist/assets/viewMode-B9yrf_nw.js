01877f9354a5f1a6b7080f5c4e9e51af74486239
yeonalbus <yeon.albus@gmail.com>
2026-08-22 20:18:31 +0800

    chore(build): 同步内嵌前端构建产物到 backend/webui/dist（Round20）

---
+++ backend/webui/dist/assets/viewMode-B9yrf_nw.js
@@ -0,0 +1,1 @@
+import{B as e,X as t}from"./_plugin-vue_export-helper-Cvkktjkp.js";import{o as n,s as r}from"./useUI-BBDrz5Ln.js";var i=t(n(`app_view_mode`,`card`));e(i,e=>{r(`app_view_mode`,JSON.stringify(e))});var a=()=>{i.value=i.value===`card`?`compact`:`card`};export{i as n,a as t};
